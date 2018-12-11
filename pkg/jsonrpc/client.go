673d843a7fa0d31168446bb5a6a4dbefc13be50c
Matthew Slipper <me@matthewslipper.com>
2018-12-11 15:54:33 -0500

    Fix race condition

--- pkg/jsonrpc/client.go
+++ pkg/jsonrpc/client.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"sync/atomic"
+	"net"
 )
 
 type Client struct {
@@ -16,17 +17,25 @@ type Client struct {
 }
 
 func NewClient(url string, timeout time.Duration) *Client {
-	defRT := http.DefaultTransport
-	defRTPtr := defRT.(*http.Transport)
-	transport := *defRTPtr
-	transport.MaxIdleConns = 100
-	transport.MaxIdleConnsPerHost = 100
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
 
 	return &Client{
 		url: url,
 		client: &http.Client{
 			Timeout:   timeout,
-			Transport: &transport,
+			Transport: transport,
 		},
 	}
 }
